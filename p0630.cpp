b7ce0e25f5dbb8fb7b84e07a7c21a3dbdd57a434
2025271328 <erickim7898@gmail.com>
2025-06-30 15:11:12 +0900

    Add files via upload

---
+++ p0630.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+//int main(void) {
+//	printf("æ»≥Á«œººø‰\n");
+//
+//
+//	return 0;
+//}
+
+//int main(void) {
+//	printf("æ»≥Á«œººø‰");
+//	return 0;
+//}
+
+//int main(void) {
+//	printf("æ»≥Á«œººø‰\næ»≥Á«œººø‰\næ»≥Á«œººø‰\n");
+//	return 0;
+//}
+
+int main(void) {
+	printf("æ»≥Á«œººø‰\n");
+	printf("æ»≥Á«œººø‰");
+	printf("æ»≥Á«œººø‰\n");
+
+	return 0;
+}
