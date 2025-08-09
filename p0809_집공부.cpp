5da9c8fb0f2d1b98199529d8fb99aefafc3caba5
2025271328 <erickim7898@gmail.com>
2025-08-09 14:40:24 +0900

    Add files via upload

---
+++ p0809_집공부.cpp
@@ -0,0 +1,154 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h> 
+//
+//struct GM {
+//	int no;
+//	char name[10];
+//	int part;
+//	int salary;
+//};
+//struct COMPANY {
+//	GM gms[5];
+//	int gmCount = 0;
+//};
+//
+//COMPANY HireCM_once(COMPANY gameCo) {
+//	gameCo.gms[gameCo.gmCount].no = gameCo.gmCount + 1;
+//	printf("이름: ");
+//	scanf(" %s", gameCo.gms[gameCo.gmCount].name);
+//	printf("부서: ");
+//	scanf("%d", &gameCo.gms[gameCo.gmCount].part);
+//	printf("월급: ");
+//	scanf("%d", &gameCo.gms[gameCo.gmCount].salary);
+//	gameCo.gmCount++;
+//	return gameCo;
+//}
+//
+//int main(void) {
+//	printf("게임을 출시해보자!\n");
+//	COMPANY gameCo;
+//
+//	printf("먼저 GM부터 고용할까?\n");
+//	while (1) {
+//		gameCo = HireCM_once(gameCo);
+//		printf("더 고용할까?(y/n) : ");
+//		char ch;
+//		do {
+//			ch = getchar();
+//		} while (ch != 'y' && ch != 'n');
+//		if (ch == 'n') break;
+//	}
+//	printf("GM %d명을 뽑았다!\n", gameCo.gmCount);
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//struct SNACK {
+//	int price;
+//	int stock;
+//	char name[20];
+//};
+//
+//void main() {
+//	const int N = 2;
+//	SNACK snacks[N] = { {1500, 3, "레몬칩"}, {1000, 2, "달고나"} };
+//
+//	printf("<<과자자판기!>>\n");
+//	for (int i = 0; i < N; i++) {
+//		printf("%d.[%6s] %4d원 (%d개남음)\n", (i + 1), snacks[i].name, snacks[i].price, snacks[i].stock);
+//	}
+//
+//	int choice;
+//	scanf("%d", &choice);
+//	printf("%s가 나왔습니다\n", snacks[choice - 1].name);
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//struct SNACK {
+//	int price;
+//	int stock;
+//	char name[20];
+//};
+//
+//void main() {
+//	const int N = 5;
+//	SNACK snacks[N] = { {1500, 3, "감자칩"}, {1000, 2, "달고나"},{1000, 3, "생수"}, {1500, 2, "콜라"}, {1000, 3, "초콜릿"}};
+//
+//	printf("<<간식자판기!>>\n");
+//	for (int i = 0; i < N; i++) {
+//		printf("%d.[%6s] %4d원 (%d개남음)\n", (i + 1), snacks[i].name, snacks[i].price, snacks[i].stock);
+//	}
+//
+//	int choice;
+//	int money;
+//	
+//	printf("지불금액을 입력해주세요! >> ");
+//	scanf("%d", &money);
+//	while (1) {
+//		printf("\n간식번호를 입력해주세요 (0 입력시 종료)>> ");
+//		scanf("%d", &choice);
+//		if (!choice) break;
+//		if (money >= snacks[choice - 1].price) {
+//			money -= snacks[choice - 1].price;
+//			printf("%s(이)가 나왔습니다.(잔여금액 : %d)\n", snacks[choice - 1].name, money);
+//		}
+//		else {
+//			printf("금액이 부족합니다. (잔여금액 : %d)\n", money);
+//		}
+//	}
+//	printf("거스름돈: %d", money);
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(void) {
+//	int* iptr = (int*)malloc(sizeof(int));
+//	char* cptr = (char*)malloc(sizeof(char));
+//	double* dptr = (double*)malloc(sizeof(double));
+//	*iptr = 5;
+//	*cptr = 'a';
+//	*dptr = 3.5;
+//	printf("%d %c %lf", *iptr, *cptr, *dptr);
+//
+//	free(iptr);
+//	free(cptr);
+//	free(dptr);
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int nRow = 5, nCol = 4;
+	int** aNum;
+
+	aNum = (int**)malloc(nRow * sizeof(int*));
+	for (int i = 0; i < nRow; i++) {
+		aNum[i] = (int*)malloc(nCol * sizeof(int));
+
+		for (int j = 0; j < nCol; j++) {
+			aNum[i][j] = i * 10 + j;
+		}
+	}
+
+	for (int i = 0; i < nRow; i++) {
+		for (int j = 0; j < nCol; j++) {
+			printf("&02d", aNum[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < nRow; i++) {
+		free(aNum[i]);
+	}
+	free(aNum);
+	return 0;
+}
